3e7a0389ed9f8267497d7ede13458c14123f8e26
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-09-28 16:26:50 +0300

    check

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import "../AuthForm/AuthForm.css"
 
 export default  class AuthForm extends Component {
+    state = {
+        userValue: '',
+        passValue: '',
+    }
+
+//    getValue = (e) => {
+//     e.preventDefault();
+//     this.setState({userValue: e.target.value})
+//     console.log(this.state.userValue)
+//    }
+
+   searchLineChangeHandler = (e) => {
+    this.setState({ userValue: e.target.value });
+    console.log(this.state.userValue)
+}
 
     render(){
         return (
             <form className="authorization">
                 <div className="authorization__title"><h2>Login</h2></div>
-                <input className="authorization__input" type="text" placeholder="Username"></input>
+                <input className="authorization__input"  onChange={this.searchLineChangeHandler} type="text" placeholder="Username"></input>
                 <input className="authorization__input" type="password" placeholder="Password"></input>
-                <button className="authorization__btn">SING IN</button>
+                <button onClick={this.searchLineChangeHandler} className="authorization__btn" type="submit">SING IN</button>
             </form>
         )
     }
